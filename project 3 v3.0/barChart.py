cc7e649343c85163cfd65f17dfa71236c68bd18f
herder19 <mister-t@hotmail.nl>
2017-04-20 11:44:41 +0200

    Update barChart.py

--- project 3 v3.0/barChart.py
+++ project 3 v3.0/barChart.py
@@ -8,8 +8,14 @@ Z = 30
 Q = 80
 T = 10
 
+X_naam = 'A1'
+Y_naam = 'A1'
+Z_naam = 'A1'
+Q_naam = 'A1'
+T_naam = 'A1'
+
 def poi():
-    objects = ('A1', 'A2', 'A3', 'A4', 'A5')
+    objects = (X_naam, Y_naam,Z_naam, Q_naam, T_naam)
     y_pos = np.arange(len(objects))
     performance = [X,Y,Z,Q,T]
  
